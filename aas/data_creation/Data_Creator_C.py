Fixed calculation error for 1ns precision rounding

aas/data_creation/Data_Creator_C.py
@@ -127,7 +127,7 @@ class Data_Creator_C(Data_Creator):
         
         #0.0001 precision - 401 classes
         if self.precision == 0.0001:
-            rounded_targets = np.asarray([np.round(abs(np.round(d * 40,2)/40), 4) for d in targets[permutation_index]]).reshape(-1)
+            rounded_targets = np.asarray([np.round(abs(np.round(d * 100,2)/100), 5) for d in targets[permutation_index]]).reshape(-1)
             classes = np.arange(0,0.04 + self.precision, self.precision)
         
         #0.00025 precision - 161 classes